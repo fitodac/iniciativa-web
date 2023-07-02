1c4be4ac90fd51e97569edccc51e8ebdcb24acc3
fitodac <fitodac@gmail.com>
2023-07-02 13:13:05 -0300

    Home page static images

--- home.php
+++ home.php
@@ -14,14 +14,53 @@ require_once('content/meta.php');
 
 		<?php require_once('components/css.php'); ?>
 		<?= $meta->ie_support; ?>
+
+		<style>
+			.homepage-landing{
+				background-color: #2D4E90;
+				width: 100%;
+				display: grid;
+			}
+
+			.homepage-landing div{
+				display: flex;
+				justify-content: center;
+			}
+
+			.homepage-landing img{
+				width: 100%;
+				max-width: 992px;
+			}
+
+			@media (min-width: 768px){
+				.homepage-landing{
+					margin-top: 160px !important;
+				}
+			}
+
+			@media (min-width: 992px){
+				.homepage-landing{
+					margin-top: 134px !important;
+				}
+			}
+		</style>
 	</head>
 	<body>
 		<?php 
 		$header_class = 'color_menu';
 		require_once('components/header.php');
-		require_once('components/hero.php');
-		require_once('components/welcome.php');
-		require_once('components/business-box.php');
+		?>
+		<div class="homepage-landing">
+			<div><img src="./img/home/01.webp" alt=""></div>
+			<div><img src="./img/home/02.webp" alt=""></div>
+			<div><img src="./img/home/03.webp" alt=""></div>
+			<div><img src="./img/home/04.webp" alt=""></div>
+		</div>
+
+		<?php 
+		// require_once('components/hero.php');
+		// require_once('components/welcome.php');
+		// require_once('components/business-box.php');
 		require_once('components/footer.php');
 		require_once('components/scripts.php');
 		?>
